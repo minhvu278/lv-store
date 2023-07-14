90c678752b07834a3de5627f948c28e698aca843
minhvu278 <33751963+minhvu278@users.noreply.github.com>
2023-07-14 23:22:07 +0700

    Add middle banner (#4)
    * Add middle banner
    
    * Edit css sp

---
+++ resources/views/pages/homepage/includes/middle-banner.blade.php
@@ -0,0 +1,36 @@
+<section id="middleBanner">
+    <div id="middleBanner__wrapper">
+        <div class="middleBanner__left">
+            <img src="{{ Vite::asset('resources/assets/client/pages/homepage/middle-banner/middle.png') }}" alt="middle">
+
+        </div>
+        <div class="middleBanner__right">
+            <div class="middleBanner__right__wrapper">
+                <div class="middleBanner__right__title">
+                    <div class="middleBanner__right__title__first">
+                        One more friend
+                    </div>
+                    <div class="middleBanner__right__title__second">
+                        Thousands more fun!
+                    </div>
+                </div>
+                <div class="middleBanner__right__description">
+                    Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!
+                </div>
+                <div class="middleBanner__right__button">
+                    <a href="#">
+                        <div class="middleBanner__right__button--first">
+                            View Intro
+                            <img src="{{ Vite::asset('resources/assets/client/pages/homepage/top-banner/video.png') }}" alt="play">
+                        </div>
+                    </a>
+                    <a href="#">
+                        <div class="middleBanner__right__button--last">
+                            Explore Now
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
