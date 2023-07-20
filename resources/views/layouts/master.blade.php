3100bd7ad6dccda07d447bcf4fe6b41d192be423
minhvu278 <dominhvu278@gmail.com>
2023-07-20 23:15:20 +0700

    fix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'Shop')</title>
-{{--    <link rel="icon" type="image/x-icon" href="{{ Vite::asset('resources/client/assets/common/favicon-32x32.ico') }}">--}}
     @vite([
         'resources/sass/client/master.scss',
         'resources/sass/client/partials/header.scss',
