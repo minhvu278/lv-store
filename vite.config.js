6771945f2d6713d0d82bc9267c56aff00987c3f8
minhvu278 <33751963+minhvu278@users.noreply.github.com>
2023-07-24 00:38:37 +0700

    Base admin lte (#11)
    * Base admin lte
    
    * Done base
    
    * Fix EOF

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 'resources/js/app.js',
+                'resources/sass/app.scss',
+                'resources/js/app.js',
 
                 // Admin
 
@@ -19,4 +21,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
